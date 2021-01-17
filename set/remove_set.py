f5018879f059ca89e73effe2ea31d9fd484332c9
DESKTOP-NGJ2KJJ\Nikom <sornrham.m@ku.th>
2021-01-17 14:19:35 +0700

    no message

---
+++ set/remove_set.py
@@ -0,0 +1,16 @@
+thislset = {"apple", "banana", "cherry"}
+thislset.remove("banana")
+print(thislset)
+
+thislset = {"apple", "banana", "cherry"}
+thislset.discard("banana")
+print(thislset)
+
+thislset = {"apple", "banana", "cherry"}
+x = thislset.pop()
+print(thislset)
+
+
+thislset = {"apple", "banana", "cherry"}
+thislset.clear()
+print(thislset)
